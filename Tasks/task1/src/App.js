218b7c106374f921f869a93eeb428e52d4f9673b
Rohith_k <2100032432cse@gmail.com>
2024-02-05 23:55:19 +0530

    addded

--- Tasks/task1/src/App.js
+++ Tasks/task1/src/App.js
@@ -1,10 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { useEffect, useRef, useState } from 'react';
+import region from './external-service';
+
 function App() {
+  const [rdata, setRdata] = useState([]);
+  const [data,setdata]=useState([])
+  const regions = region.getRegionsList();
+  const langvage=region.getLanguagesList()
+
+ const [show,setshow]=useState(false)
+ function handlereg(){
+  setRdata(regions)
+  setshow(true)
+ }
+ function handlelang(){
+  setRdata(langvage)
+  setshow(true)
+ }
+ function handlechange(e){
+let lst= region.getCountryListByRegion(e.target.value)
+if(lst.length==0)  lst= region.getCountryListByLanguage(e.target.value)
+setdata(lst)
+ }
+ console.log(data);
   return (
-    <div className="App">
-     
+    <div className='App'>
+      <h1>Countries Search</h1>
+      <input type="radio" name="region" id=""  onClick={handlereg}/> region
+      <input type="radio" name="language" id="" onClick={handlelang} />langvage
+
+      <select  onChange={handlechange} disabled={!show}>
+        {show?
+        rdata.map((e) => (
+          <option key={e} value={e}>
+            {e}
+          </option>
+        )):<option value={'empty'} >empty</option>
+      }
+        
+      </select>
+{data.map((e)=>([
+  <p>{JSON.stringify(e)}</p>
+]))}
     </div>
   );
 }
